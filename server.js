38eabc8f9dca15267c75d43a0daabb6be583fedc
usagi123 <maipham.quanghuy@gmail.com>
2018-10-04 10:23:40 +0700

    update port

--- server.js
+++ server.js
@@ -2,4 +2,6 @@ const http = require('http');
 const app = require('./app');
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
-server.listen(port)
+server.listen(port, () => {
+    console.log("App is running on port " + port);
+});
